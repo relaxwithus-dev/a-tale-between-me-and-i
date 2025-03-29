b469bccf2b5445c23d6885974835f180a960504d
rokuta <restudooctavriandi@gmail.com>
2025-03-29 13:34:35 +0700

    fix: bug inventory selector

--- Assets/_Project/Scripts/Inventory/InventoryHandler.cs
+++ Assets/_Project/Scripts/Inventory/InventoryHandler.cs
@@ -72,7 +72,10 @@ namespace ATBMI.Inventory
 
         private void CloseInventoryMenu()
         {
-            // (_inventoryCreator.InventoryFlags[selectedIndex] as InventoryFlag)?.Highlight(false);
+            if (selectedIndex != -1)
+            {
+                (_inventoryCreator.InventoryFlags[selectedIndex] as InventoryFlag)?.Highlight(false);
+            }
 
             selectedIndex = -1;
         }
