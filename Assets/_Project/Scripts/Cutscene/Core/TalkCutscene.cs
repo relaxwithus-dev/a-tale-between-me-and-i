d4806ea76480bf13743b42dedf156c1fe3a39880
Shinon <zyuuannn@gmail.com>
2025-05-13 04:27:44 +0700

    feat: debug mode talk cutscene

--- Assets/_Project/Scripts/Cutscene/Core/TalkCutscene.cs
+++ Assets/_Project/Scripts/Cutscene/Core/TalkCutscene.cs
@@ -9,28 +9,49 @@ namespace ATBMI.Cutscene
         #region Fields
         
         [Header("Stats")]
+        [SerializeField] private bool debugMode;
+        [SerializeField] private string debugText;
         [SerializeField] private TextAsset dialogueText;
+        
+        private bool _isStarted;
         
         // Reference
         private DialogueManager _dialogueManager;
-
+        
         #endregion
-
+        
         #region Methods
         
-        protected override void InitOnAwake()
+        protected override void InitOnStart()
         {
-            base.InitOnAwake();
+            base.InitOnStart();
             _dialogueManager = DialogueManager.Instance;
         }
         
         public override void Execute()
         {
+            if (debugMode)
+            {
+                if (_isStarted) return;
+                Debug.Log($"execute talk: {debugText}");
+                _isStarted = true;
+            }
+            
             if (_dialogueManager.IsDialoguePlaying) return;
             DialogueEvents.EnterDialogueEvent(dialogueText);
         }
 
-        public override bool IsFinished() => !_dialogueManager.IsDialoguePlaying;
+        public override bool IsFinished()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _isStarted = false;
+                return true;
+            }
+
+            return false;
+            return !_dialogueManager.IsDialoguePlaying;
+        }
         
         #endregion
     }
