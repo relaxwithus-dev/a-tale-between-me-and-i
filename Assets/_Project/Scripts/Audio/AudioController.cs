894647f8f0e30d880b338d071c93808400418013
Shinon <zyuuannn@gmail.com>
2025-05-18 23:22:59 +0700

    feat: audio controller each scene

---
+++ Assets/_Project/Scripts/Audio/AudioController.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+
+namespace ATBMI.Audio
+{
+    public class AudioController : MonoBehaviour
+    {
+        #region Fields & Properties
+
+        [Header("Stats")]
+        [SerializeField] private Musics musicName;
+        
+        // Event
+        public static event Action<bool, float> OnFadeAudio;
+
+        #endregion
+
+        #region Methods
+
+        // Unity Callbacks
+        private void OnEnable()
+        {
+            OnFadeAudio += FadeAudio;
+        }
+
+        private void OnDisable()
+        {
+            OnFadeAudio -= FadeAudio;
+        }
+        
+        private void Start()
+        {
+            FadeAudioEvent(isFadeIn: true);
+        }
+        
+        // Core
+        public static void FadeAudioEvent(bool isFadeIn, float duration = 0.5f)
+        {
+            OnFadeAudio?.Invoke(isFadeIn, duration);
+        }
+        
+        private void FadeAudio(bool isFadeIn, float duration)
+        {
+            var audio = AudioManager.Instance.GetAudio(musicName);
+            
+            StartCoroutine(isFadeIn
+                ? AudioHelpers.FadeIn(audio.source, duration, audio.volume)
+                : AudioHelpers.FadeOut(audio.source, duration));
+        }
+        
+        #endregion
+    }
+}
---
+++ Assets/_Project/Scripts/Audio/AudioHelpers.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+
+namespace ATBMI.Audio
+{
+    public static class AudioHelpers
+    {
+        public static IEnumerator CrossFade(this AudioSource fromAudio, AudioSource toAudio, 
+            float targetVolume, float fadeTime)
+        {
+            yield return FadeOut(fromAudio, fadeTime);
+            yield return FadeIn(toAudio, fadeTime, targetVolume);
+        }
+        
+        public static IEnumerator FadeOut(this AudioSource audio, float fadeTime)
+        {
+            var startVolume = audio.volume;
+            while (audio.volume > 0)
+            {
+                audio.volume -= startVolume * Time.deltaTime / fadeTime;
+                yield return null;
+            }
+            audio.Stop();
+            audio.volume = 0;
+        }
+
+        public static IEnumerator FadeIn(this AudioSource audio, float fadeTime, float targetVolume)
+        {
+            var startVolume = 0.2f;
+            audio.volume = 0;
+            audio.Play();
+            while (audio.volume < targetVolume)
+            {
+                audio.volume += startVolume * Time.deltaTime / fadeTime;
+                yield return null;
+            }
+            audio.volume = targetVolume;
+        }
+    }
+}
