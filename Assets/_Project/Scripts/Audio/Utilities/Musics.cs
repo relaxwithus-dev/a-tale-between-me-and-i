d9d101536e217ef4e7f495ef2a843e6bfcdd2048
Satrio Septiaji <satriosprat@gmail.com>
2025-05-21 08:28:50 +0700

    feat: adding musics id

--- Assets/_Project/Scripts/Audio/Utilities/Musics.cs
+++ Assets/_Project/Scripts/Audio/Utilities/Musics.cs
@@ -3,6 +3,8 @@ namespace ATBMI.Audio
     public enum Musics
     {
         BGM_MainMenu, BGM_Bali, BGM_Surabaya, BGM_Bandung,
+        BGM_CS1, BGM_CS2, BGM_CS3, BGM_CS4, BGM_CS5,
+        BGM_CS6, BGM_CS7,
         SFX_Footstep, SFX_Interact, SFX_Button, SFX_Typing,
         SFX_Phone, SFX_TabPhone, SFX_ChangeUI,
         None
